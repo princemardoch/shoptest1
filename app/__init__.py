843375a9664ee2e7d75ff361af56ffb87b31a850
leprincemardoch <doublepo56@gmail.com>
2024-09-06 03:24:51 +0000

    Edit product

--- app/__init__.py
+++ app/__init__.py
@@ -16,14 +16,8 @@ def create_app():
     app.config['SESSION_COOKIE_HTTPONLY'] = True
     app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
-    from .user_test1.userr import user1
-    from .user_test2.user2 import user2 
-    from .user_test3.userr import user3
-    from .user_test4.userr import user4 
+    from .user.userr import user
 
-    app.register_blueprint(user1, url_prefix='/user1')
-    app.register_blueprint(user2, url_prefix='/t2')
-    app.register_blueprint(user3, url_prefix='/t3')
-    app.register_blueprint(user4, url_prefix='/t4')
+    app.register_blueprint(user, url_prefix='/')
 
     return app
